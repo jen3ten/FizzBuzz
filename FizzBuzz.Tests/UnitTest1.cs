a6c9bd8f2144cd17ae84b542f556dd548a7a1551
Mark Palmer <mark.palmer@itsalllive.com>
2019-05-23 20:53:02 -0400

    first commit

---
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Get_Returns_1_for_1()
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var result = fizzBuzz.Get(1);
+
+            Assert.Equal("1", result);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string Get(int num)
+        {
+            return "1";
+        }
+    }
+}
